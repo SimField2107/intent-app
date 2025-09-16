8cd7e7d16ac7023e544ef1e26c38fc4613611080
Seline Cornfield <Seline2107@googlemail.com>
2025-09-16 17:51:02 +0100

    feat(ui): Integrate Menu and Timer into Activity View

--- src/components/ActivityView.jsx
+++ src/components/ActivityView.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
+import Menu from './Menu';
+import Timer from './Timer';
 import styles from './ActivityView.module.css';
 
 const ActivityView = () => {
@@ -13,10 +15,10 @@ const ActivityView = () => {
         &larr; Back to Board
       </button>
       <h1 className={styles.title}>{activeCard.title}</h1>
-      <p>
-        {/* This is where our tools like the Menu and Timer will go later. */}
-        Activity content for this section will be here.
-      </p>
+      <div className={styles.toolsContainer}>
+        <Menu />
+        <Timer />
+      </div>
     </div>
   );
 };
